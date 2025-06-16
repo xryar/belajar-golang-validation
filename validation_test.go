8993d3377961f2a964162dafeb661da26cb1fd6c
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-06-16 09:37:09 +0700

    parameter tag

--- validation_test.go
+++ validation_test.go
@@ -45,3 +45,13 @@ func TestMultipleTag(t *testing.T) {
 		fmt.Println(err.Error())
 	}
 }
+
+func TestTagParameter(t *testing.T) {
+	validate := validator.New()
+	user := "12345"
+
+	err := validate.Var(user, "required,number,min=5,max=10")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
