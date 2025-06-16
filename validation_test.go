db39fc313adc599e68534594a68f7e158eec5119
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-06-16 08:57:46 +0700

    first commit

---
+++ validation_test.go
@@ -0,0 +1,14 @@
+package belajargolangvalidation
+
+import (
+	"testing"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func TestValidation(t *testing.T) {
+	validate := validator.New()
+	if validate == nil {
+		t.Error("Validate is nil")
+	}
+}
