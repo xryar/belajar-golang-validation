7a1fc3fade008443173f6363f9d844576a6e3274
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-06-17 14:11:06 +0700

    another cross field validation

--- validation_test.go
+++ validation_test.go
@@ -428,3 +428,39 @@ func TestOrRule(t *testing.T) {
 		fmt.Println(err.Error())
 	}
 }
+
+func MustEqualsIgnoreCase(field validator.FieldLevel) bool {
+	value, _, _, ok := field.GetStructFieldOK2()
+	if !ok {
+		panic("field is not ok")
+	}
+
+	firstValue := strings.ToUpper(field.Field().String())
+	secondValue := strings.ToUpper(value.String())
+
+	return firstValue == secondValue
+}
+
+func TestCrossField(t *testing.T) {
+	validate := validator.New()
+	validate.RegisterValidation("field_equals_ignore_case", MustEqualsIgnoreCase)
+
+	type User struct {
+		Username string `validate:"required,field_equals_ignore_case=Email|field_equals_ignore_case=Phone"`
+		Email    string `validate:"required,email"`
+		Phone    string `validate:"required,numeric"`
+		Name     string `validate:"required"`
+	}
+
+	user := User{
+		Username: "123456",
+		Email:    "acumalaka@example.com",
+		Phone:    "123456",
+		Name:     "Acumalaka",
+	}
+
+	err := validate.Struct(user)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
